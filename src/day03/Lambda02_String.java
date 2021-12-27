2027fb31d62a61014d22a43c4789e03c7c8365f3
Omer Tufan <omertufanca@gmail.com>
2021-12-27 22:29:53 +0100

    27.12.21

--- src/day03/Lambda02_String.java
+++ src/day03/Lambda02_String.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 import day01.Lambda01;
 import day01.Lambda02_Filter;
@@ -13,7 +14,7 @@ public class Lambda02_String {
 
 	public static void main(String[] args) {
 		
-		List<String> list=new ArrayList<>(Arrays.asList("mehmet", "emre", "nilgun", "yildiz", "kader", "emine", "islam", "islam", "emre"));
+		List<String> list=new ArrayList<>(Arrays.asList("kokorec","mehmet", "emre", "nilgun", "yildiz", "kader", "emine", "islam", "islam", "emre"));
 		
 		siraBykTkrsiz(list);
 		System.out.println(" ");
@@ -24,6 +25,14 @@ public class Lambda02_String {
 		sonHarfTers(list);
 		System.out.println(" ");
 		ciftKareTekrarsizMintoMax(list);
+		System.out.println(" ");
+		yediKrkterKontrol(list);
+		System.out.println(" ");
+		wIleBaslama(list);
+		System.out.println(" ");
+		xIleBiten(list);
+		System.out.println(" ");
+		karakterMax(list);
 	}
 	
 	//list elemanlarini alfabetik buyuk harf ve tekrarsiz yazdiriniz
@@ -64,5 +73,47 @@ public class Lambda02_String {
 		sorted(Comparator.reverseOrder()).forEach(Lambda01::printEl);
 				
 	}
+	//List elelmmalarinin karakter sayisini 7 ve 7 'den az olma durumunu kontrol ediniz
+	public static void yediKrkterKontrol(List<String> list) {
+		
+		boolean kontrol = list.stream().map(String::length).allMatch(t->t<=7);
+		System.out.println(kontrol?"List elemanlari 7 den buyuk degil" : " List elemanlarindan 7 den buyuk var");
+		
+		//System.out.println(list.stream().map(t->t.length()>7 ? "List elemanlari 7 den buyuk degil" : "List elemanlarindan 7 den buyuk var"));
+	}
 	
+	//List elemanlarinin "W" ile baslamasini kontrol ediniz
+	
+	public static void wIleBaslama(List<String> list) {
+		
+	boolean kontrol = list.stream().noneMatch(t->t.toLowerCase().startsWith("w"));
+		
+		System.out.println(kontrol?"Elemanlardan W ile baslayan yok" : "Elemanlardan  W ile baslayan var" );
+	}
+	
+	//List elemanlarinin "x" ile biten en az bir elemani kontrol ediniz
+	
+		public static void xIleBiten(List<String> list) {
+			
+		boolean kontrol = list.stream().anyMatch(t->t.toLowerCase().endsWith("x"));
+			
+			System.out.println(kontrol?"Elemanlardan x ile biten var" : "Elemanlardan x ile biten yok" );
+		}
+		
+		//anyMatch() --> enaz bir eleman sarti saglarsa true aksi durumda false return eder
+		//allMatch() --> tum  elemanlar sarti saglarsa true en az bir eleman sarti saglamazsa false return eder.
+		//noneMatch() --> hic bir sarti SAGLAMAZSA true en az bir eleman sarti SAGLARSA false return eder
+		
+		//karakter sayisi en buyuk elemani yazdiriniz
+		public static void karakterMax(List<String> list) {
+			
+			Optional<String> max = list.stream().sorted(Comparator.comparing(String::length).reversed()).findFirst();
+			System.out.println(max);
+			
+			
+		}
+		
+		
+		
+		
 }
