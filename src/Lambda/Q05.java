1b156cd603ebb65ae759f37a9811f2085f958196
Omer Tufan <omertufanca@gmail.com>
2022-01-12 22:30:24 +0100

    12.01.22

--- src/Lambda/Q05.java
+++ src/Lambda/Q05.java
@@ -1,6 +1,23 @@
 package Lambda;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Q05 {
     //         {"A", "B", "D", "C", "B", "A", "A", "B", "B"} gibi bir array oluşturun;
 //         Tekrarlanan öğeleri konsolda yazdırın
+	public static void main(String[] args) {
+		
+		List<String> list=new ArrayList<>(Arrays.asList("A", "B", "D", "C", "B", "A", "A", "B", "B"));
+		System.out.println(list);
+		
+		list.stream()
+        .filter(t -> Collections.frequency(list, t) > 1)
+        .collect(Collectors.toSet())
+        .forEach(t -> System.out.println(t));
+		
+	}
 }
--- src/Lambda/Q06.java
+++ src/Lambda/Q06.java
@@ -1,7 +1,27 @@
 package Lambda;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public class Q06 {
-    //Fonksiyonel Programlama ile [12, 9, 13, 4, 6, 2, 4, 12, 15] listesindeki tüm tek sayıları yazdırın
+    
     //6 ile 14 arasindaki tum sayilarin karesini ekrana yazdiriniz
     //Listteki tek tamsayilarin karekoklerinin toplamini ekrana yazdiriniz
+	
+	public static void main(String[] args) {
+		
+		List<Integer> list=new ArrayList<>(Arrays.asList(12, 9, 13, 4, 6, 2, 4, 12, 15));
+		
+		//Fonksiyonel Programlama ile [12, 9, 13, 4, 6, 2, 4, 12, 15] listesindeki tüm tek sayıları yazdırın
+		
+		list.stream().filter(t->t%2==1).forEach(t->System.out.print(t+" "));
+		
+		 //6 ile 14 arasindaki tum sayilarin karesini ekrana yazdiriniz
+		
+		
+		
+		
+	}
 }
