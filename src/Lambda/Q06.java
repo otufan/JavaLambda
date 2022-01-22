7f6b51fd6d4376a77f4efb3e02927fed68b031e4
Omer Tufan <omertufanca@gmail.com>
2022-01-22 21:29:18 +0100

    22.01.22

--- src/Lambda/Q06.java
+++ src/Lambda/Q06.java
@@ -13,15 +13,16 @@ public class Q06 {
 	public static void main(String[] args) {
 		
 		List<Integer> list=new ArrayList<>(Arrays.asList(12, 9, 13, 4, 6, 2, 4, 12, 15));
+		System.out.println("***************");
 		
 		//Fonksiyonel Programlama ile [12, 9, 13, 4, 6, 2, 4, 12, 15] listesindeki tüm tek sayıları yazdırın
 		
-		list.stream().filter(t->t%2==1).forEach(t->System.out.print(t+" "));
-		
+		list.stream().filter(t->t%2==1).forEach(t->System.out.println(t+" "));
+		System.out.println("***************");
 		 //6 ile 14 arasindaki tum sayilarin karesini ekrana yazdiriniz
 		
-		
-		
+		list.stream().filter(t->t<14&&t>6).sorted().map(t->t*t).forEach(t->System.out.println(t + " "));
+		System.out.println("***************");
 		
 	}
 }
